9c01836a4a413a4cedf38575df0cbad73c8ddde6
Harrison Goldstein <hgoldstein95@gmail.com>
2019-07-13 11:51:35 -0400

    Updates

---
+++ precache-manifest.d2901bb571430f3cb714545c32b679c2.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee52d90e5fc2a596d21b364553a65b5",
+    "url": "/tourist-web/css.worker.js"
+  },
+  {
+    "revision": "7ac2f10b7936ad10580f2186e1ae8544",
+    "url": "/tourist-web/editor.worker.js"
+  },
+  {
+    "revision": "3a2030c41c1717edcf8ed9fe8dffff92",
+    "url": "/tourist-web/html.worker.js"
+  },
+  {
+    "revision": "14d11b74269a507aa47f03d2ac28c8b4",
+    "url": "/tourist-web/index.html"
+  },
+  {
+    "revision": "9af12a07202a8c1d1a09abde168afe5d",
+    "url": "/tourist-web/json.worker.js"
+  },
+  {
+    "revision": "6c67c66436d8adfe0a0d",
+    "url": "/tourist-web/static/css/3.a174f98a.chunk.css"
+  },
+  {
+    "revision": "819a65864cd370e463f4",
+    "url": "/tourist-web/static/css/main.d7d17a55.chunk.css"
+  },
+  {
+    "revision": "c4e72252a54ed5b1fb5e",
+    "url": "/tourist-web/static/js/0.ba614319.chunk.js"
+  },
+  {
+    "revision": "de43559e82c6f78dbfb7",
+    "url": "/tourist-web/static/js/10.2d766504.chunk.js"
+  },
+  {
+    "revision": "6bc319c0f572c3ee0933",
+    "url": "/tourist-web/static/js/11.7f5e27c6.chunk.js"
+  },
+  {
+    "revision": "af80af53666de7855e5b",
+    "url": "/tourist-web/static/js/12.7bd855dd.chunk.js"
+  },
+  {
+    "revision": "8e09ad035f30d005e70f",
+    "url": "/tourist-web/static/js/13.344ad975.chunk.js"
+  },
+  {
+    "revision": "a1c917f5e5cadad600ff",
+    "url": "/tourist-web/static/js/14.162838e8.chunk.js"
+  },
+  {
+    "revision": "cd33ae3b2f2770977b6e",
+    "url": "/tourist-web/static/js/15.d34351e0.chunk.js"
+  },
+  {
+    "revision": "03e55b20c12954cc83b2",
+    "url": "/tourist-web/static/js/16.6d01ab02.chunk.js"
+  },
+  {
+    "revision": "5c9a530d0c9f85cdb5a7",
+    "url": "/tourist-web/static/js/17.9e707748.chunk.js"
+  },
+  {
+    "revision": "903332d187b9d6ccfa29",
+    "url": "/tourist-web/static/js/18.311b18ab.chunk.js"
+  },
+  {
+    "revision": "9fe59079865f20eb9283",
+    "url": "/tourist-web/static/js/19.5a021b0a.chunk.js"
+  },
+  {
+    "revision": "027cc03f1cabd15aa11c",
+    "url": "/tourist-web/static/js/20.c2913f20.chunk.js"
+  },
+  {
+    "revision": "5d3625ec265b307cd646",
+    "url": "/tourist-web/static/js/21.fea55994.chunk.js"
+  },
+  {
+    "revision": "380b84272bffc98a22cc",
+    "url": "/tourist-web/static/js/22.ef686bfb.chunk.js"
+  },
+  {
+    "revision": "712243ab8a3f82856dc0",
+    "url": "/tourist-web/static/js/23.9f5d3b6c.chunk.js"
+  },
+  {
+    "revision": "23af73e774e37c0be827",
+    "url": "/tourist-web/static/js/24.9bbeceda.chunk.js"
+  },
+  {
+    "revision": "cfd2c87763d79d9e0654",
+    "url": "/tourist-web/static/js/25.e24f120b.chunk.js"
+  },
+  {
+    "revision": "3ebef565c8cecb19babf",
+    "url": "/tourist-web/static/js/26.5684d4df.chunk.js"
+  },
+  {
+    "revision": "feb3e27b0bd05e403370",
+    "url": "/tourist-web/static/js/27.c0202a9c.chunk.js"
+  },
+  {
+    "revision": "89b593785e1ccea9e90c",
+    "url": "/tourist-web/static/js/28.444b3cc9.chunk.js"
+  },
+  {
+    "revision": "450bc3b50e78b1da71b4",
+    "url": "/tourist-web/static/js/29.bf2f6188.chunk.js"
+  },
+  {
+    "revision": "6c67c66436d8adfe0a0d",
+    "url": "/tourist-web/static/js/3.59aa9fc4.chunk.js"
+  },
+  {
+    "revision": "68948af0b8228bccfad7",
+    "url": "/tourist-web/static/js/30.d3e52e1f.chunk.js"
+  },
+  {
+    "revision": "e4e60efdfd0e1f68909a",
+    "url": "/tourist-web/static/js/31.c33d9054.chunk.js"
+  },
+  {
+    "revision": "7f7a1738381948cda1d2",
+    "url": "/tourist-web/static/js/32.8a39b7c6.chunk.js"
+  },
+  {
+    "revision": "f3a5bd03c98b95f65614",
+    "url": "/tourist-web/static/js/33.99e64dc7.chunk.js"
+  },
+  {
+    "revision": "3b8cb2348951b44dd73f",
+    "url": "/tourist-web/static/js/34.362e476a.chunk.js"
+  },
+  {
+    "revision": "99db8f80a0086dd6310a",
+    "url": "/tourist-web/static/js/35.a649cc2a.chunk.js"
+  },
+  {
+    "revision": "f46909307cf4fe4c3231",
+    "url": "/tourist-web/static/js/36.4ca69624.chunk.js"
+  },
+  {
+    "revision": "3388ba7362e5f3a36030",
+    "url": "/tourist-web/static/js/37.4b4168ea.chunk.js"
+  },
+  {
+    "revision": "66ac391e226100abe956",
+    "url": "/tourist-web/static/js/38.3f9a8ab4.chunk.js"
+  },
+  {
+    "revision": "8081d57c6a5937cd3c1d",
+    "url": "/tourist-web/static/js/39.e27e2fbb.chunk.js"
+  },
+  {
+    "revision": "5711c82730d10728f0bb",
+    "url": "/tourist-web/static/js/4.a7214d90.chunk.js"
+  },
+  {
+    "revision": "6df9cb6c0dfa581f7b9d",
+    "url": "/tourist-web/static/js/40.7eb22d4a.chunk.js"
+  },
+  {
+    "revision": "e5c811965a36243acf6d",
+    "url": "/tourist-web/static/js/41.c3bab9aa.chunk.js"
+  },
+  {
+    "revision": "cf9e3fbcdc4018255099",
+    "url": "/tourist-web/static/js/42.e6bd6ab7.chunk.js"
+  },
+  {
+    "revision": "6e820832f26613b9f16c",
+    "url": "/tourist-web/static/js/43.1aedbb56.chunk.js"
+  },
+  {
+    "revision": "43deb3f0838dae792d19",
+    "url": "/tourist-web/static/js/44.c072aa0f.chunk.js"
+  },
+  {
+    "revision": "b8b4603732fdff83385b",
+    "url": "/tourist-web/static/js/45.df72782b.chunk.js"
+  },
+  {
+    "revision": "e7afc16c6a2532dc940c",
+    "url": "/tourist-web/static/js/46.77c9eb3e.chunk.js"
+  },
+  {
+    "revision": "5958b2a20c1d6939f6e6",
+    "url": "/tourist-web/static/js/47.bad6bf1d.chunk.js"
+  },
+  {
+    "revision": "c88aa0d734ad6b7ef72d",
+    "url": "/tourist-web/static/js/48.4d8ff805.chunk.js"
+  },
+  {
+    "revision": "c5e7aee55b103a5c8de2",
+    "url": "/tourist-web/static/js/49.da708003.chunk.js"
+  },
+  {
+    "revision": "d8043d606bbc31aa852a",
+    "url": "/tourist-web/static/js/5.a5890e2e.chunk.js"
+  },
+  {
+    "revision": "1b734e3fa00160a6ea65",
+    "url": "/tourist-web/static/js/50.101a896e.chunk.js"
+  },
+  {
+    "revision": "364a2193571465991362",
+    "url": "/tourist-web/static/js/51.4fb64e5f.chunk.js"
+  },
+  {
+    "revision": "d5a44ceea3c0f9801278",
+    "url": "/tourist-web/static/js/52.2ede546b.chunk.js"
+  },
+  {
+    "revision": "b3c50d274dba96b1b561",
+    "url": "/tourist-web/static/js/53.e2d635d2.chunk.js"
+  },
+  {
+    "revision": "2b8db97c410ec624b932",
+    "url": "/tourist-web/static/js/54.46917b85.chunk.js"
+  },
+  {
+    "revision": "da894e1ec0a3cb3b3c7d",
+    "url": "/tourist-web/static/js/55.83b8b364.chunk.js"
+  },
+  {
+    "revision": "746779f4c744964163ab",
+    "url": "/tourist-web/static/js/6.1f4cda40.chunk.js"
+  },
+  {
+    "revision": "318848db601778d60ba2",
+    "url": "/tourist-web/static/js/7.0dce3150.chunk.js"
+  },
+  {
+    "revision": "0c3076a467612b00adac",
+    "url": "/tourist-web/static/js/8.6e4ffaa4.chunk.js"
+  },
+  {
+    "revision": "d55f536219d3dfe836a3",
+    "url": "/tourist-web/static/js/9.5b891a30.chunk.js"
+  },
+  {
+    "revision": "819a65864cd370e463f4",
+    "url": "/tourist-web/static/js/main.02d07c00.chunk.js"
+  },
+  {
+    "revision": "5bf0cc170328cbd1b3d8",
+    "url": "/tourist-web/static/js/runtime~main.2c48fa64.js"
+  },
+  {
+    "revision": "ae297e51f4f62a158f22c07a2cd60c64",
+    "url": "/tourist-web/typescript.worker.js"
+  }
+]);
